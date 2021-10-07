511bd85fac7b3f9023b2ed9ae406277b963bbe03
vladan345 <vladanm087@gmail.com>
2021-10-07 14:30:33 +0200

    Added birthday functionality

---
+++ README.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - Launch countdown timer solution
+
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for all interactive elements on the page
+- See a live countdown timer that ticks down every second (start the count at 14 days)
+- **Bonus**: When a number changes, make the card flip from the middle
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Solution](https://github.com/vladan345/birthday-countdown)
+- Live Site URL: [Live Preview](https://vladan345.github.io/birthday-countdown/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- SCSS
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JS
+
+### What I learned
+
+Learned how to compare and use Date() object, how to animate flip-flop animation.
+
+### Continued development
+
+First, this started as an animation challenge, simple countdown function wasn't too hard, but then I had an idea to make this countdown as a birthday reminder (I have a bad memory for my friend's birthdays). So really, for now, I'll wait and see if everything works and maybe optimize and write cleaner code, other than that, is pretty much a finished project.
+
+## Author
+
+- Website - [Vladan345](https://github.com/vladan345)
+- Frontend Mentor - [@vladan345](https://www.frontendmentor.io/profile/vladan345)
--- app.js
+++ app.js
@@ -1,5 +1,114 @@
-var end = new Date('2021-10-30T23:47:00');
+var year = new Date().getFullYear();
+var newDate = '';
+var newName = '';
 
+let birthdays = [
+  {
+    year: year,
+    name: 'Tara',
+    date: `${this.year}-01-19T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Dusan(Avala)',
+    date: `${this.year}-02-27T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Natalija',
+    date: `${this.year}-03-29T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Vladan',
+    date: `${this.year}-04-10T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Jana',
+    date: `${this.year}-08-01T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Filip',
+    date: `${this.year}-08-20T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Nixa',
+    date: `${this.year}-09-02T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Alex',
+    date: `${this.year}-10-16T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Tonic',
+    date: `${this.year}-10-21T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Flora',
+    date: `${this.year}-11-28T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Dimi',
+    date: `${this.year}-12-04T00:00:00`,
+  },
+  {
+    year: year,
+    name: 'Dusan(Souly) and Toza',
+    date: `${this.year}-12-12T00:00:00`,
+  },
+];
+
+var counter = 0;
+//IF COUNTER IS EQUALS TO NUMBER OF BIRTHDAYS, THAT MEANS
+//THERE ARE NO BIRTHDAYS LEFT THIS YEAR.
+//IN THAT CASE WE NEED TO CHANGE CURRENT YEAR SO THE PROGRAM
+//CAN CHECK NEXT BIRTHDAY IN THE BEGGINING OF THE NEXT YEAR.
+function getBirthday(array) {
+  loopDates(array);
+  if (counter == array.length) {
+    year++;
+    let newArray = changeYear([...array]);
+    loopDates(newArray);
+  }
+}
+getBirthday(birthdays);
+
+//THIS CHECKS IF BIRTHAY HAS PASSED, IF YES, IT ADDS 1 TO COUNTER
+//IF NOT, IT MEANS THAT BIRTHDAY IS NEXT UP.
+function loopDates(dateArray) {
+  var today = new Date();
+  for (let i = 0; i < dateArray.length; i++) {
+    var checkDate = new Date(dateArray[i].date);
+    var choosenName = dateArray[i].name;
+    if (checkDate > today) {
+      newDate = checkDate;
+      newName = choosenName;
+      break;
+    } else {
+      counter++;
+    }
+  }
+}
+
+//THIS FUNCTION MAKES NEW ARRAY WITH ALL DATES WITH NEW YEAR.
+function changeYear(oldArray) {
+  for (let i = 0; i < oldArray.length; i++) {
+    var newDate = year + oldArray[i].date.slice(-15);
+    oldArray[i].date = newDate;
+    oldArray[i].year = year;
+  }
+  return oldArray;
+}
+
+document.querySelector('h1').innerHTML = `It's ${newName}'s birthday soon`;
+var end = new Date(newDate);
 var units = ['day', 'hour', 'minute', 'second'];
 
 var second = 1000;
